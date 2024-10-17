8660809ed5cd8b3481f4899b83ff664ab597e0ae
Yunochi <yuno@yunochi.com>
2024-10-17 17:56:34 +0900

    Feat: 외부 파일 접근시 재캐싱 기능
    - Fix: 원격의 민감한 미디어 캐싱 설정을 존중하도록
    - Fix: lock 과 db를 얻기 전에 캐싱이 켜져있는지부터 확인
    - refactor: Remote 파일 Re-Cache에 Queue 사용

--- packages/backend/src/queue/QueueProcessorModule.ts
+++ packages/backend/src/queue/QueueProcessorModule.ts
@@ -43,6 +43,7 @@ import { TickChartsProcessorService } from './processors/TickChartsProcessorServ
 import { AggregateRetentionProcessorService } from './processors/AggregateRetentionProcessorService.js';
 import { ExportFavoritesProcessorService } from './processors/ExportFavoritesProcessorService.js';
 import { RelationshipProcessorService } from './processors/RelationshipProcessorService.js';
+import { ReDownloadRemoteFileProcessorService } from './processors/ReDownloadRemoteFileProcessorService.js';
 
 @Module({
 	imports: [
@@ -88,6 +89,7 @@ import { RelationshipProcessorService } from './processors/RelationshipProcessor
 		CheckModeratorsActivityProcessorService,
 		CleanRemoteNotesProcessorService,
 		QueueProcessorService,
+		ReDownloadRemoteFileProcessorService,
 	],
 	exports: [
 		QueueProcessorService,
