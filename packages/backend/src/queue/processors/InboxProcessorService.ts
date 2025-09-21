0cc48a1f9375c7af2e9f62f9a5a7fea795bf5764
Wonwoo Choi <chwo9843@gmail.com>
2025-09-21 16:21:14 +0900

    fix(backend): Properly handle too many mentions error in inbox

--- packages/backend/src/queue/processors/InboxProcessorService.ts
+++ packages/backend/src/queue/processors/InboxProcessorService.ts
@@ -232,6 +232,9 @@ export class InboxProcessorService implements OnApplicationShutdown {
 				if (e.id === '85ab9bd7-3a41-4530-959d-f07073900109') {
 					return 'actor has been suspended';
 				}
+				if (e.id === '9f466dab-c856-48cd-9e65-ff90ff750580') {
+					return 'note contains too many mentions';
+				}
 				if (e.id === 'd450b8a9-48e4-4dab-ae36-f4db763fda7c') { // invalid Note
 					return e.message;
 				}
