afe823f15c09c388af5f6d99903ba2eb1a97bb5b
Wonwoo Choi <chwo9843@gmail.com>
2025-09-21 16:15:37 +0900

    feat(backend): Add config `autoSelectFamilyAttemptTimeout`

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -90,6 +90,7 @@ type Source = {
 
 	outgoingAddress?: string;
 	outgoingAddressFamily?: 'ipv4' | 'ipv6' | 'dual';
+	autoSelectFamilyAttemptTimeout?: number;
 
 	deliverJobConcurrency?: number;
 	inboxJobConcurrency?: number;
@@ -160,6 +161,7 @@ export type Config = {
 	id: string;
 	outgoingAddress: string | undefined;
 	outgoingAddressFamily: 'ipv4' | 'ipv6' | 'dual' | undefined;
+	autoSelectFamilyAttemptTimeout?: number;
 	deliverJobConcurrency: number | undefined;
 	inboxJobConcurrency: number | undefined;
 	relationshipJobConcurrency: number | undefined;
@@ -295,6 +297,7 @@ export function loadConfig(): Config {
 		clusterLimit: config.clusterLimit,
 		outgoingAddress: config.outgoingAddress,
 		outgoingAddressFamily: config.outgoingAddressFamily,
+		autoSelectFamilyAttemptTimeout: config.autoSelectFamilyAttemptTimeout,
 		deliverJobConcurrency: config.deliverJobConcurrency,
 		inboxJobConcurrency: config.inboxJobConcurrency,
 		relationshipJobConcurrency: config.relationshipJobConcurrency,
