803b601dd4fdf0a750aee934f903a8a5419064d5
Wonwoo Choi <chwo9843@gmail.com>
2026-05-02 17:11:54 +0900

    Patch `sass-embedded` for Nix compat

--- nix/build.nix
+++ nix/build.nix
@@ -4,6 +4,7 @@
   fetchPnpmDeps,
   pnpmConfigHook,
 
+  dart-sass,
   nodejs,
   pnpm,
 
@@ -64,6 +65,7 @@ stdenv.mkDerivation (finalAttrs: {
   strictDeps = true;
 
   nativeBuildInputs = [
+    dart-sass
     nodejs
     python3
     pkg-config
@@ -81,6 +83,8 @@ stdenv.mkDerivation (finalAttrs: {
     fetcherVersion = 3;
     hash = "";
   };
+
+  SASS_EMBEDDED_BIN_PATH = "${dart-sass}/bin/sass";
 
   postPatch = ''
     export HOME=$PWD
